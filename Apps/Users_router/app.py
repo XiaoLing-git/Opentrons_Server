41d3f7b68ead8cc5a41dc97c3d81eabace8a88c7
XiaoLing-git <40679757+XiaoLing-git@users.noreply.github.com>
2021-07-08 10:42:39 +0800

    pip  install redis

--- Apps/Users_router/app.py
+++ Apps/Users_router/app.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from typing import List
 
 from fastapi import Depends,  HTTPException
 from sqlalchemy.orm import Session
@@ -30,15 +31,8 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return User.create_user(db=db, user=user)
 
 
-# @user_app.get("/users/", response_model=List[schemas.User])
-# def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     users = User.get_users(db, skip=skip, limit=limit)
-#     return users
-#
-#
-# @user_app.get("/users/{user_id}", response_model=schemas.User)
-# def read_user(user_id: int, db: Session = Depends(get_db)):
-#     db_user = User.get_user(db, user_id=user_id)
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     return db_user
+@user_app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = User.get_users(db, skip=skip, limit=limit)
+    return users
+
--- Apps/Users_router/dependencies/User.py
+++ Apps/Users_router/dependencies/User.py
@@ -3,20 +3,16 @@ from sqlalchemy.orm import Session
 from .. import models,schemas
 
 
-# def get_user(db: Session, user_id: int):
-#     return db.query(models.User).filter(models.User.id == user_id).first()
-#
-
 def get_user_by_phone(db: Session, phone: str):
     return db.query(models.User).filter(models.User.u_phone == phone).first()
-#
-#
-# def get_users(db: Session, skip: int = 0, limit: int = 100):
-#     return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.u_password + "notreallyhashed"
+    fake_hashed_password = user.u_password
     db_user = models.User(u_phone=user.u_phone, u_password=fake_hashed_password)
     db.add(db_user)
     db.commit()
