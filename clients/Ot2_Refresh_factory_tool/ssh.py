635a9c2ba16ab9f71bc8ecca194eee64bae414fe
XiaoLing-git <40679757+XiaoLing-git@users.noreply.github.com>
2021-08-25 16:27:47 +0800

    fixed ssh error

--- clients/Ot2_Refresh_factory_tool/ssh.py
+++ clients/Ot2_Refresh_factory_tool/ssh.py
@@ -5,7 +5,7 @@ from api.interactive import interactive_shell
 def main()->None:
     with robot_api.find() as robot:
         print(f"SSHing into {robot.hostname_or_ip}...")
-        interactive_shell(robot.start_shell)
+        interactive_shell(robot.start_shell())
 
 
 if __name__ == '__main__':
